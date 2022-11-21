86589662961b1bd11b15a4e40b45421c887fdb7f
cllojan <cesar.lojan8@gmail.com>
2022-11-21 15:55:21 -0500

    timezone now job

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -1,18 +1,19 @@
 from django.conf import settings
+from django.utils.timezone import now
 from datetime import datetime
 from registrar.models import Empleado
 
 def delete_data():    
-    try:
-        aa = datetime.now()
-        asd = str(aa).split(".")[0]
-        xd = asd[0:16]
-        data = Empleado.objects.filter(end_time=xd)
+    try:        
+        conv = str(now()).split(".")[0]
+        expir = conv[0:16]        
+        data = Empleado.objects.filter(end_time=expir)
         data.delete()
         
     except Empleado.DoesNotExist as e:
         print(e)
-        
+    except Exception as ex:
+        print(e)
     
 
 
